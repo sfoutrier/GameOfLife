0f6bb8ea73588018832bb32f6b632da6fc6dee5b
Sebastien Foutrier <sebastien.foutrier@gmail.com>
2017-12-10 14:48:44 +0100

    First version

---
+++ GameOfLife/MainForm.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace GameOfLife
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxGrid = new System.Windows.Forms.PictureBox();
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.NumericPace = new System.Windows.Forms.NumericUpDown();
+            this.randBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericPace)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxGrid
+            // 
+            this.pictureBoxGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxGrid.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxGrid.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxGrid.Name = "pictureBoxGrid";
+            this.pictureBoxGrid.Size = new System.Drawing.Size(864, 666);
+            this.pictureBoxGrid.TabIndex = 0;
+            this.pictureBoxGrid.TabStop = false;
+            this.pictureBoxGrid.SizeChanged += new System.EventHandler(this.PictureBoxGrid_SizeChanged);
+            this.pictureBoxGrid.Click += new System.EventHandler(this.PictureBoxGrid_Click);
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.StartBtn.Location = new System.Drawing.Point(12, 684);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(402, 40);
+            this.StartBtn.TabIndex = 1;
+            this.StartBtn.Text = "Start";
+            this.StartBtn.UseVisualStyleBackColor = true;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // NumericPace
+            // 
+            this.NumericPace.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.NumericPace.Location = new System.Drawing.Point(756, 692);
+            this.NumericPace.Name = "NumericPace";
+            this.NumericPace.Size = new System.Drawing.Size(120, 26);
+            this.NumericPace.TabIndex = 2;
+            this.NumericPace.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.NumericPace.ValueChanged += new System.EventHandler(this.NumericPace_ValueChanged);
+            // 
+            // randBtn
+            // 
+            this.randBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.randBtn.Location = new System.Drawing.Point(587, 684);
+            this.randBtn.Name = "randBtn";
+            this.randBtn.Size = new System.Drawing.Size(163, 40);
+            this.randBtn.TabIndex = 3;
+            this.randBtn.Text = "Randomize";
+            this.randBtn.UseVisualStyleBackColor = true;
+            this.randBtn.Click += new System.EventHandler(this.RandBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.clearBtn.Location = new System.Drawing.Point(421, 685);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(160, 39);
+            this.clearBtn.TabIndex = 4;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.ClearBtn_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(888, 736);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.randBtn);
+            this.Controls.Add(this.NumericPace);
+            this.Controls.Add(this.StartBtn);
+            this.Controls.Add(this.pictureBoxGrid);
+            this.Name = "MainForm";
+            this.Text = "Game of Life";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericPace)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxGrid;
+        private System.Windows.Forms.Button StartBtn;
+        private System.Windows.Forms.NumericUpDown NumericPace;
+        private System.Windows.Forms.Button randBtn;
+        private System.Windows.Forms.Button clearBtn;
+    }
+}
+
---
+++ GameOfLife/MainForm.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+using Timer = System.Threading.Timer;
+
+namespace GameOfLife
+{
+    public partial class MainForm : Form
+    {
+        private bool _started = false;
+        private Timer _updateTimer;
+        private TimeSpan _updatePeriod = TimeSpan.FromSeconds(1);
+        private int _width = 100;
+        private int _height = 100;
+        private bool[,] _grid;
+        private bool[,] _backupGrid;
+        private Bitmap _bitmap;
+
+        private static readonly Color LiveColor = Color.Black;
+        private static readonly Color DeadColor = Color.White;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void StartStop()
+        {
+            if (_started)
+            {
+                StartBtn.Text = "Start";
+                _started = false;
+            }
+            else
+            {
+                StartBtn.Text = "Stop";
+                _started = true;
+                ReArmTimer();
+            }
+        }
+
+        private void RandBtn_Click(object sender, EventArgs e)
+        {
+            int numProb = 1;
+            int denumProb = 8;
+
+            var rand = new Random();
+            UpdateWith((x, y) => rand.Next(denumProb) <= numProb - 1);
+
+            BuildImg();
+            AdjustImage();
+        }
+
+        private void ClearBtn_Click(object sender, EventArgs e)
+        {
+            UpdateWith((x, y) => false);
+        }
+
+        private void UpdateWith(Func<int, int, bool> predicate)
+        {
+            if (_started)
+                StartStop();
+
+            for (int x = 0; x < _width; ++x)
+                for (int y = 0; y < _height; ++y)
+                    _grid[x, y] = predicate(x, y);
+
+            BuildImg();
+            AdjustImage();
+        }
+
+        private void BuildImg()
+        {
+            var img = new Bitmap(_width, _height);
+            for (int x = 0; x < _width; ++x)
+                for (int y = 0; y < _height; ++y)
+                    img.SetPixel(x, y, _grid[x, y] ? LiveColor : DeadColor);
+            _bitmap = img;
+        }
+
+        private void AdjustImage()
+        {
+            if (_bitmap != null)
+            {
+                Bitmap result = new Bitmap(pictureBoxGrid.ClientSize.Width, pictureBoxGrid.ClientSize.Height);
+                int xResolution = pictureBoxGrid.ClientSize.Width / _width;
+                int yResolution = pictureBoxGrid.ClientSize.Width / _height;
+                using (Graphics g = Graphics.FromImage(result))
+                {
+                    g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+                    g.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.Half;
+                    lock (_bitmap)
+                        g.DrawImage(_bitmap, 0, 0, pictureBoxGrid.ClientSize.Width, pictureBoxGrid.ClientSize.Height);
+                }
+                pictureBoxGrid.Image = result;
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            _updateTimer = new Timer(OnTimerTick);
+            _grid = new bool[_width, _height];
+            _backupGrid = new bool[_width, _height];
+            BuildImg();
+            AdjustImage();
+        }
+
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            if (NumericPace.Value == 0 && !_started)
+                MessageBox.Show("You can't start with 0 image per second");
+            else
+                StartStop();
+        }
+
+        private void NumericPace_ValueChanged(object sender, EventArgs e)
+        {
+            if (NumericPace.Value == 0 && _started)
+                StartStop();
+            else
+            {
+                _updatePeriod = TimeSpan.FromSeconds(1 / (double)NumericPace.Value);
+                ReArmTimer();
+            }
+        }
+
+        private void ReArmTimer(TimeSpan when = default(TimeSpan))
+        {
+            _updateTimer.Change(_started ? when : Timeout.InfiniteTimeSpan, Timeout.InfiniteTimeSpan);
+        }
+
+        private void PictureBoxGrid_SizeChanged(object sender, EventArgs e)
+        {
+            AdjustImage();
+        }
+
+        private void PictureBoxGrid_Click(object sender, EventArgs e)
+        {
+            if (e is MouseEventArgs evt)
+            {
+                int x = _width * evt.Location.X / pictureBoxGrid.ClientSize.Width;
+                int y = _height * evt.Location.Y / pictureBoxGrid.ClientSize.Height;
+                {
+                    // left makes the cell alive, others kills it
+                    var liveness = _grid[x, y] = (evt.Button == MouseButtons.Left);
+                    lock (_bitmap)
+                        _bitmap.SetPixel(x, y, liveness ? LiveColor : DeadColor);
+                }
+                AdjustImage();
+            }
+        }
+
+        private void OnTimerTick(object _)
+        {
+            UpdateLiveness();
+            ReArmTimer(_updatePeriod);
+        }
+
+        private void UpdateLiveness()
+        {
+            for (int x = 0; x < _width; ++x)
+                for (int y = 0; y < _height; ++y)
+                {
+                    _backupGrid[x, y] = IsAlive(x, y);
+                    if (_backupGrid[x, y] != _grid[x, y]) lock(_bitmap)
+                        _bitmap.SetPixel(x, y, _backupGrid[x, y] ? LiveColor: DeadColor);
+                }
+            var toBackup = _grid;
+            _grid = _backupGrid;
+            _backupGrid = toBackup;
+            AdjustImage();
+        }
+
+        private bool IsAlive(int x, int y)
+        {
+            int liveNeigbors = NeighborsOf(x, y).Count(p => _grid[p.X, p.Y]);
+            if (liveNeigbors == 2)
+                return _grid[x, y];
+
+            return liveNeigbors == 3;
+        }
+
+        private IEnumerable<Point> NeighborsOf(int x, int y)
+        {
+            if (x > 0)
+            {
+                if (y > 0)
+                    yield return new Point(x - 1, y - 1);
+                yield return new Point(x - 1, y);
+                if (y < _height - 1)
+                    yield return new Point(x - 1, y + 1);
+            }
+
+            if (y > 0)
+                yield return new Point(x, y - 1);
+            if (y < _height - 1)
+                yield return new Point(x, y + 1);
+
+            if (x < _width - 1)
+            {
+                if (y > 0)
+                    yield return new Point(x + 1, y - 1);
+                yield return new Point(x + 1, y);
+                if (y < _height - 1)
+                    yield return new Point(x + 1, y + 1);
+            }
+        }
+    }
+}
